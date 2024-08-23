45243c643dfc9d79f4a5a238dca9444ba5df1071
ZakTan <Ztanksley@luc.edu>
2024-08-23 11:53:50 -0700

    Initial Commit

---
+++ CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,33 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by Zakee Tanksley on 8/22/24.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+    // outlets -  gives access the properties of object
+    // action - says do something to be done
+    @IBOutlet var cardImageView: UIImageView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+    }
+    @IBAction func restartButtonTapped(_ sender: Any) {
+    }
+    @IBAction func rulesButtonTapped(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+}
